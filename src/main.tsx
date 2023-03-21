e62386c747d2ac8d05804ea9baf0ff5f03c780a2
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-03-21 18:31:34 +0100

    :sparkles: Created store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './style.css';
+import StoreProvider from './providers/storeProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <StoreProvider>
+            <App />
+        </StoreProvider>
     </React.StrictMode>
 );
